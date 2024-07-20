b1a393817730fb8c64dd3bc9b22fc350049c78c2
sid <165424989+matrSid@users.noreply.github.com>
2024-07-20 21:10:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ async function generateImages(input) {
     const prompt = `${input} ${randomNumber}`;
     // We added random number to prompt to create different results
     const response = await fetch(
-        "https://api-inference.huggingface.co/models/xinsir/controlnet-union-sdxl-1.0",
+        "https://api-inference.huggingface.co/models/prompthero/openjourney",
         {
             method: "POST",
             headers: {
